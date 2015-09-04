067ee311fe39e7e00cb2f32c7eb108314eb28ec2
Milton Mazzarri <milmazz@gmail.com>
2015-09-04 01:32:41 -0500

    Plugin improvements
    This commit include the following:
    
     * Update options for js-beautify
     * Update all the dependencies for development
     * Replace JSHint with ESLint
     * Improvements in the Gruntfile.js script
     * Adapt all the JS to the standard code style

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,58 +1,56 @@
 module.exports = function (grunt) {
+  // load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
+  require('load-grunt-tasks')(grunt)
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc'
-            },
-            highcharts: 'highcharts-show-last-points.js',
-            gruntfile: 'Gruntfile.js'
-        },
-        jsbeautifier: {
-            highcharts: {
-                src: 'highcharts-show-last-points.js',
-                options: {
-                    config: '.jsbeautifyrc'
-                }
-            },
-            gruntfile: {
-                src: 'Gruntfile.js',
-                options: {
-                    config: '.jsbeautifyrc'
-                }
-            }
-        },
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-                    '/* <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-                separator: ',',
-                compress: true,
-            },
-            highcharts: {
-                src: 'highcharts-show-last-points.js',
-                dest: 'highcharts-show-last-points.min.js'
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    eslint: {
+      options: {
+        configFile: '.eslintrc'
+      },
+      highcharts: 'highcharts-show-last-points.js',
+      gruntfile: 'Gruntfile.js'
+    },
+    jsbeautifier: {
+      highcharts: {
+        src: 'highcharts-show-last-points.js',
+        options: {
+          config: '.jsbeautifyrc'
         }
-    });
+      },
+      gruntfile: {
+        src: 'Gruntfile.js',
+        options: {
+          config: '.jsbeautifyrc'
+        }
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+          '/* <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+        separator: ',',
+        compress: true
+      },
+      highcharts: {
+        src: 'highcharts-show-last-points.js',
+        dest: 'highcharts-show-last-points.min.js'
+      }
+    }
+  })
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-jsbeautifier');
-
-    // Tasks
-    grunt.registerTask('default', [
-        'jsbeautifier:highcharts',
-        'jshint:highcharts'
-    ]);
-    grunt.registerTask('gruntfile', [
-        'jsbeautifier:gruntfile',
-        'jshint:gruntfile'
-    ]);
-    grunt.registerTask('dist', [
-        'jsbeautifier:highcharts',
-        'jshint:highcharts',
-        'uglify:highcharts'
-    ]);
-};
+  // Tasks
+  grunt.registerTask('default', [
+    'jsbeautifier:highcharts',
+    'eslint:highcharts'
+  ])
+  grunt.registerTask('gruntfile', [
+    'jsbeautifier:gruntfile',
+    'eslint:gruntfile'
+  ])
+  grunt.registerTask('dist', [
+    'jsbeautifier:highcharts',
+    'eslint:highcharts',
+    'uglify:highcharts'
+  ])
+}
